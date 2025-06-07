f487bf13bed18483cc0fa37e78a4d61885214d96
Martín Fenocchio <fenomartin6@gmail.com>
2025-06-07 11:05:35 -0300

    feat: add contacts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SolanaModule } from './solana/solana.module';
 import { UsersModule } from './users/users.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [SolanaModule, UsersModule],
+  imports: [SolanaModule, UsersModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
