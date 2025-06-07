bdfdd795aac5726d83646236920b67f3f7db98f8
Martín Fenocchio <fenomartin6@gmail.com>
2025-06-07 11:04:33 -0300

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SolanaModule } from './solana/solana.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [SolanaModule],
+  imports: [SolanaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
